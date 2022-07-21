52bb7454392fc70b1c3c5eb06c864822d26590af
MilenK <milen_kunchev@abv.bg>
2022-07-21 12:30:19 +0300

    06_queens_puzzle

---
+++ Recursion and Backtracking/06_queens_puzzle.py
@@ -0,0 +1,15 @@
+row = ['-'] * 8
+puzzle = [row] * 8
+positions = set()
+
+
+def print_result(result):
+    for r in puzzle:
+        print(*r)
+
+
+def queen_puzzle(row, col, puzzle, positions):
+    pass
+
+
+queen_puzzle(0, 0, puzzle, positions)
